f16ccd490a80270023c39141dff7dd3d2d318b10
d11210920 <d11210920@naver.com>
2025-07-27 14:53:41 +0900

    1

--- src/main/java/com/example/practice/HomeController.java
+++ src/main/java/com/example/practice/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/")
     public String home() {
-        return "this is a practice for ci-cd";
+        return "this is a practice for ci-cd with blue-green";
     }
 }
